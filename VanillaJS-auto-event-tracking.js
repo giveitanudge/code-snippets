4a12d6b9a20d1f10e9acda4b6a5819deba2f583a
Izac Hancock <izac01@gmail.com>
2022-02-24 12:19:09 -0700

    Update VanillaJS-auto-event-tracking.js

--- VanillaJS-auto-event-tracking.js
+++ VanillaJS-auto-event-tracking.js
@@ -1,3 +1,6 @@
+// The below code needs to be added AFTER the Nudge tracking snippet to ensure it is registered to the page correctly. 
+// Remember to wrap the code in <script> tags when adding into a .html, .php or other non .js type file. 
+
 document.getElementById("nudge-track").addEventListener("click", function() {
   ndg(this.getAttribute('data-nudge'));
 });
